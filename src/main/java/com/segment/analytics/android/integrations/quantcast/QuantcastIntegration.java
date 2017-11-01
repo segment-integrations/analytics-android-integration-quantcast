b2e968d6f04161974b38ef5400d5d488fc1d6148
Brennan Gamwell <brennan@segment.com>
2017-11-01 13:45:55 -0700

    Update/dependencies and apply java-fmt-plugin (#4)
    * Apply javafmt plugin
    * Bump gradle deps and re-import gradle wrapper to resolve IDE errors

--- src/main/java/com/segment/analytics/android/integrations/quantcast/QuantcastIntegration.java
+++ src/main/java/com/segment/analytics/android/integrations/quantcast/QuantcastIntegration.java
@@ -19,27 +19,30 @@ import static com.segment.analytics.Analytics.LogLevel.VERBOSE;
  * @see <a href="https://www.quantcast.com/">Quantcast</a>
  * @see <a href="https://segment.com/docs/integrations/quantcast/">Quantcast Integration</a>
  * @see <a href="https://github.com/quantcast/android-measurement#quantcast-android-sdk">Quantcast
- * Android SDK</a>
+ *     Android SDK</a>
  */
 public class QuantcastIntegration extends Integration<Void> {
   private static final String QUANTCAST_KEY = "Quantcast";
   private static final String VIEWED_EVENT_FORMAT = "Viewed %s Screen";
 
-  public static final Factory FACTORY = new Factory() {
-    @Override public Integration<?> create(ValueMap settings, Analytics analytics) {
-      String apiKey = settings.getString("apiKey");
-      Logger logger = analytics.logger(QUANTCAST_KEY);
-      if (logger.logLevel == VERBOSE) {
-        logger.verbose("QuantcastClient.enableLogging(true);");
-        QuantcastClient.enableLogging(true);
-      }
-      return new QuantcastIntegration(apiKey, logger);
-    }
+  public static final Factory FACTORY =
+      new Factory() {
+        @Override
+        public Integration<?> create(ValueMap settings, Analytics analytics) {
+          String apiKey = settings.getString("apiKey");
+          Logger logger = analytics.logger(QUANTCAST_KEY);
+          if (logger.logLevel == VERBOSE) {
+            logger.verbose("QuantcastClient.enableLogging(true);");
+            QuantcastClient.enableLogging(true);
+          }
+          return new QuantcastIntegration(apiKey, logger);
+        }
 
-    @Override public String key() {
-      return QUANTCAST_KEY;
-    }
-  };
+        @Override
+        public String key() {
+          return QUANTCAST_KEY;
+        }
+      };
 
   final String apiKey;
   final Logger logger;
@@ -49,31 +52,36 @@ public class QuantcastIntegration extends Integration<Void> {
     this.logger = logger;
   }
 
-  @Override public void onActivityStarted(Activity activity) {
+  @Override
+  public void onActivityStarted(Activity activity) {
     super.onActivityStarted(activity);
     logger.verbose("QuantcastClient.activityStart(activity, %s, null, null);", apiKey);
     QuantcastClient.activityStart(activity, apiKey, null, null);
   }
 
-  @Override public void onActivityStopped(Activity activity) {
+  @Override
+  public void onActivityStopped(Activity activity) {
     super.onActivityStopped(activity);
     logger.verbose("QuantcastClient.activityStop();");
     QuantcastClient.activityStop();
   }
 
-  @Override public void identify(IdentifyPayload identify) {
+  @Override
+  public void identify(IdentifyPayload identify) {
     super.identify(identify);
     String userId = identify.userId();
     logger.verbose("QuantcastClient.recordUserIdentifier(%s);", userId);
     QuantcastClient.recordUserIdentifier(userId);
   }
 
-  @Override public void screen(ScreenPayload screen) {
+  @Override
+  public void screen(ScreenPayload screen) {
     super.screen(screen);
     logEvent(String.format(VIEWED_EVENT_FORMAT, screen.event()));
   }
 
-  @Override public void track(TrackPayload track) {
+  @Override
+  public void track(TrackPayload track) {
     super.track(track);
     logEvent(track.event());
   }
